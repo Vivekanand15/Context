e20ae569a3529a27b802d9739bb01d5e2af3824f
Vivekanand <vivekanand.v@surya-soft.com>
2022-03-02 12:23:08 +0530

    Accessing data with Consumer

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+
 
 class App extends React.Component{
     state={language:'english'};
     onLanguageChange=(language)=>{
         this.setState({language});
         console.log("hello");
-    }
+    };
 
     render(){
         return(
@@ -16,7 +18,9 @@ class App extends React.Component{
                     <i className="flag us"  onClick={()=>this.onLanguageChange('english')}/>
                 <i className="flag nl" onClick={()=>this.onLanguageChange('dutch')} />
                 </div>
+                <LanguageContext.Provider value={this.state.language}>
                 <UserCreate/>
+                </LanguageContext.Provider>
             </div>
         );
     };
